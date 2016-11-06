76d4543860bdd6d47ca0ed09b063a33adc151bd4
highmember <ftn2539@gmail.com>
2016-11-06 20:09:18 +0700

    ขึ้น complete เรียบร้อย เหลืออีกหน่อยช่วยดกูหน่อย

--- app.js
+++ app.js
@@ -2,25 +2,14 @@ angular.module( 'Todos',[])
   .service('Services',function(){
     var self = this
 
-<<<<<<< HEAD
     self.Message = [ { mes: ''}]
-    self.check = [{ ch: ''}]
-=======
-    self.Message = [ { mes: 'weef'}]
->>>>>>> 67969ce2eb223e5099e5159ec36517ee838f9b77
 
     self.list = function() {return self.Message}
-
     self.add = function(Message) {self.Message.push(Message)}
 })
 
   .controller('ListMessageController',function ($scope, Services){
     $scope.Message = Services.list()
-    $scope.checkc = function(){
-      var c = {ch: $scope.ch}
-      Services.add(c)
-    }
-
 })
 
   .controller('AddMessageController', function($scope, Services){
