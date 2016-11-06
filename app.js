222b4d77dccf2200e2ca8d552d2c0dd8eb84dd8f
highmember <ftn2539@gmail.com>
2016-11-06 15:57:20 +0700

    DEMO 1 เหลือทำ complete

---
+++ app.js
@@ -0,0 +1,24 @@
+angular.module( 'Todos',[])
+  .service('Services',function(){
+    var self = this
+
+    self.Message = [ { mes: 'ไก่อ๊อฟ'}]
+
+    self.list = function() {return self.Message}
+
+    self.add = function(Message) {self.Message.push(Message)}
+})
+
+  .controller('ListMessageController',function ($scope, Services){
+    $scope.Message = Services.list()
+})
+
+  .controller('AddMessageController', function($scope, Services){
+    $scope.save = function(){
+      var m = {mes: $scope.mes}
+      Services.add(m)
+      resetForm()
+    }
+
+    function resetForm(){$scope.mes =''}
+})
